9d545d56f77715b54dfc9372a8cc59c372c754ce
Mariana Viana <immarianaviana@gmail.com>
2022-01-31 20:29:46 -0300

    chore: add routes

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import {
 
 
 import { GlobalStyle} from './utils/globalStyle'
+
 import { Navigate } from 'react-router-dom'
 import { useContext } from 'react'
+
 import Character from './pages/Character'
+import Characters from './pages/Characters'
 import CurrentFilm from './pages/Film'
 import List from './pages/Films'
+import Planet from './pages/Planet'
+import Planets from './pages/Planets'
 import StoreContext from './components/store/Context'
 import StoreProvider from './components/store/Provider'
 import UserLogin from './pages/Login'
@@ -33,6 +38,9 @@ const App = () => {
           <Route path="/film/:id/*" element={<PrivateRoute><CurrentFilm /></PrivateRoute>}/>
           <Route path="/login" element={<UserLogin/>} />
           <Route path="/people/:id/*" element={<PrivateRoute><Character /></PrivateRoute>}/>
+          <Route path="/people/" element={<PrivateRoute><Characters /></PrivateRoute>}/>
+          <Route path="/planet/:id/*" element={<PrivateRoute><Planet /></PrivateRoute>}/>
+          <Route path="/planets/" element={<PrivateRoute><Planets /></PrivateRoute>}/>
           <Router path="*" element={<h1>Página não encontrada :(</h1>} />
         </Routes>
       </StoreProvider>
